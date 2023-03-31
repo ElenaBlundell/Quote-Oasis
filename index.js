ff631030911c73e01fff5671689a1cfd6b1982ff
ElenaBlundell <blundell.elena@gmail.com>
2023-03-31 11:30:39 -0500

    Styling changes

--- index.js
+++ index.js
@@ -3,46 +3,63 @@ const get = element => document.getElementById(element);
 const open = get("menu-btn")
 const nav = get("nav")
 const exit = get("exit-btn")
+const searchBar = get("search-bar")
+const searchContainer = get("search-container")
+
+const searchBtn = get("search-btn")
+
 const main = get("main")
 
-let quotesArr = []
-
-open.addEventListener('click', () => {
-    nav.classList.add('open-nav')
-})
-
-exit.addEventListener('click', () => {
-    nav.classList.remove('open-nav')
-})
-
+const quoteBlock = get("quote-block")
 const quoteBtn = get("get-quote-btn")
 
-function getNewQuote() {
-    const nextQuote = quotesArr.shift()
-    return nextQuote ? getQuoteHtml(nextQuote) : {}
-}
-
-   function getQuoteHtml(data) {
-        return `
-         <div id="quote-block">
-            <p class="quote">"${data.q}"</p>
-            <div class="author">
-                <img src="images/palm.png">
-            <p>${data.a}</p>
-            </div>
-        </div>`
-    }
-
-
-quoteBtn.addEventListener("click", () => {
-    if (quotesArr > 0){
-        main.innerHTML = getNewQuote()
-    }else{
-    fetch ("https://zenquotes.io/api/quotes")
-      .then(response => response.json())
-      .then(data => {quotesArr = data
-    console.log(quotesArr)
-    main.innerHTML = getNewQuote()
-    })}
+open.addEventListener("click", () => {
+    nav.classList.add("open-nav")
+    quoteBtn.style.opacity = "1"
+    searchContainer.style.visibility = "hidden"
+    searchBtn.style.visibility = "visible"
 })
 
+exit.addEventListener("click", () => {
+    nav.classList.remove('open-nav')
+    quoteBtn.style.opacity = "0.5"
+})
+
+searchBtn.addEventListener("click", () => {
+    searchBtn.style.visibility = "hidden"
+    searchContainer.style.visibility = "visible"
+} )
+
+// function getAuthorImg(data){
+//     let authorImgSource = ""
+//     let author = data.author
+//     let authorFetch = author.replace(/ /g, "%20")
+//     console.log(authorFetch)
+//     fetch(`https://en.wikipedia.org/w/api.php?action=query&
+//                 titles=${authorFetch}&formatversion=2&prop=pageimages&format=json&pithumbsize=250`)
+//             .then(response => response.json())
+//             .then(img => {
+//                 console.log(img.query.pages[0].thumbnail.source)
+//                 authorImgSource = img.query.pages[0].thumbnail.source
+//             })
+//             return authorImgSource
+// }
+
+function getQuoteHtml(data) {
+    // let authorImg = getAuthorImg(data)
+        return `
+            <p class="quote">"${data.content}"</p>
+            <div class="author">
+                <img src="images/palm.png">
+            <p>${data.author}</p>
+            </div>`
+    }
+
+quoteBtn.addEventListener("click", () => {
+    fetch ("https://api.quotable.io/random")
+      .then(response => response.json())
+      .then(data => {
+        console.log(data)
+        quoteBlock.innerHTML = getQuoteHtml(data)
+    })
+})
